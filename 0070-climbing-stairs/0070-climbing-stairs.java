68eed2bb599049046a337b9e5afe0f455f54619b
RevanthBalineni-14 <89599670+RevanthBalineni-14@users.noreply.github.com>
2023-08-27 22:22:07 -0400

    Time: 0 ms (100.00%), Space: 39 MB (83.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,20 @@
 class Solution {
     
     public int climbStairs(int n) {
-        int[] arr=new int[n];
         if(n==1)
             return 1;
         if(n==2)
             return 2;
         int count=2;
-        arr[0]=1;
-        arr[1]=2;
+        int one=1;
+        int two=2;
+        int crt=0;
         while(count<n){
-            arr[count]=arr[count-1]+arr[count-2];
+            crt=one+two;
+            one=two;
+            two=crt;
             count++;
         }
-        return arr[count-1];
+        return crt;
     }
 }
