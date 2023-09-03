7e37ff3dda17839c36882652eaececca011e801e
RevanthBalineni-14 <89599670+RevanthBalineni-14@users.noreply.github.com>
2023-09-03 00:14:56 -0400

    Time: 21 ms (88.89%), Space: 54.9 MB (11.13%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,16 +1,7 @@
 class Solution {
+    
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int []> q=new PriorityQueue<>((p1,p2)->(-p1[1]*p1[1]-p1[0]*p1[0]+p2[1]*p2[1]+p2[0]*p2[0]));
-        for(int i=0;i<points.length;i++){
-            q.offer(points[i]);
-            if(q.size()>k)
-                q.poll();
-        }
-        int[][] result=new int[k][2];
-        while(k>0){
-            k--;
-            result[k]=q.poll();
-        }
-        return result;
+        Arrays.sort(points,(pb,pa)->pb[0]*pb[0]+pb[1]*pb[1]-pa[0]*pa[0]-pa[1]*pa[1]);
+        return Arrays.copyOfRange(points,0,k);
     }
 }
