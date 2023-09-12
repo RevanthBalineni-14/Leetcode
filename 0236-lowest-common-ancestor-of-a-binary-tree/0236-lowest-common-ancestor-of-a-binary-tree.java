748e1eac91f9e7e1cb43b418efa7dea106e9fe8b
RevanthBalineni-14 <89599670+RevanthBalineni-14@users.noreply.github.com>
2023-09-12 02:46:30 -0400

    Time: 6 ms (98.31%), Space: 43.4 MB (65.03%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,42 +8,19 @@
  * }
  */
 class Solution {
-    List<Integer> result=new ArrayList<Integer>();
-    private  boolean search(TreeNode target,TreeNode root){
-        if(root==null)
-            return false;
-        if(root.val==target.val)
-        {
-            result.add(root.val);
-            return true;
-        }
-        if(search(target, root.left))
-        {
-            result.add(root.val);
-            return true;
-        }
-        else if(search(target, root.right)){
-            result.add(root.val);
-            return true;
-        }
-        return false;
-    }
+    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        search(p,root);
-        List<Integer> lp=new ArrayList<Integer>(result);
-        result.clear();
-        search(q,root);
-        List<Integer> lq=result;
-        Collections.reverse(lp);
-        Collections.reverse(lq);
-        int count=0;
-        while(count<lp.size()&&count<lq.size()&&lp.get(count).equals(lq.get(count)))
-        {
-            System.out.println(lp.get(count)+"    "+lq.get(count));
-            count++;
+        if(root==null||root.val==p.val||root.val==q.val){
+            return root;
         }
-        if(count<lp.size()&&count<lq.size())
-        System.out.println(lp.get(count)+"    "+lq.get(count));
-        return new TreeNode(lp.get(count-1));
+        TreeNode left=lowestCommonAncestor(root.left, p, q);
+        TreeNode right=lowestCommonAncestor(root.right, p, q);
+        if(right==null)
+            return left;
+
+        if(left==null)
+            return right;
+        
+        return root;
     }
 }
