fa29e23d43d795b9bb33b97862389ed1fbb9a4f1
RevanthBalineni-14 <89599670+RevanthBalineni-14@users.noreply.github.com>
2024-01-07 08:50:03 -0600

    Time: 375 ms (29.11%), Space: 14.1 MB (93.54%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+
+        dp = [10001]*n
+        dp[n-1] = 0
+
+        for i in range(n-2, -1 ,-1):
+            dp[i] = min(dp[i : min(nums[i]+i+1, n)]) + 1
+        
+        return dp[0]
