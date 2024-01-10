e7ab89f2c9d40810d16b76b44c7abfb3581ec34d
RevanthBalineni-14 <89599670+RevanthBalineni-14@users.noreply.github.com>
2024-01-10 08:51:07 -0600

    Time: 642 ms (90.00%), Space: 161.9 MB (60.00%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def amountOfTime(self, root, start):
+        """
+        :type root: Optional[TreeNode]
+        :type start: int
+        :rtype: int
+        """
+        res = [0]  # Use a list to make 'res' mutable
+        ct = [0]   # Use a list to make 'ct' mutable
+
+        def depth(root):
+            if not root:
+                return 0
+            return max(depth(root.left), depth(root.right)) + 1
+
+        def find(root):
+            if not root:
+                return False
+
+            if root.val == start:
+                print("Hoi", depth(root.left), depth(root.right))
+                res[0] = max(depth(root.left), depth(root.right))
+                return True
+
+            if find(root.left):
+                ct[0] += 1
+                res[0] = max(res[0], ct[0] + depth(root.right))
+                return True
+
+            if find(root.right):
+                ct[0] += 1
+                res[0] = max(res[0], ct[0] + depth(root.left))
+                return True
+
+            return False
+
+        find(root)
+        return res[0]
