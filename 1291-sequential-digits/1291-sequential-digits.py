24bfe34047c7853afa4995ba6c56d34683ed14bc
RevanthBalineni-14 <89599670+RevanthBalineni-14@users.noreply.github.com>
2024-02-01 23:39:25 -0600

    Time: 28 ms (91.30%), Space: 16.5 MB (63.35%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -5,15 +5,11 @@ class Solution:
         increment = [0, 0, 11, 111, 1111, 11111, 111111, 1111111, 11111111, 111111111]
         for i in range(len(str(low)), min(len(str(high)), 9)+1):
             crt = start[i]
-            print(crt)
             while crt<=high:
-                print("Hi", crt)
-                if crt<low:
-                    crt += increment[i]
-                    if crt%10==0:
-                        break
-                    continue
-                res.append(crt)
+                
+                if crt>=low:
+                    res.append(crt)
+                    
                 crt += increment[i]
                 if crt%10==0:
                     break
