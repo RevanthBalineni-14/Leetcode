0fdfb0f16bc0df56056fb6725cf37846e07af86e
RevanthBalineni-14 <89599670+RevanthBalineni-14@users.noreply.github.com>
2024-02-03 20:45:08 -0500

    Time: 83 ms (74.23%), Space: 15.2 MB (38.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,33 +1,35 @@
-class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        if t=="":
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        if t == "":
             return ""
-        tcount = {}
-        window = {}
-        for c in t:
-            tcount[c] = 1 + tcount.get(c,0)
-        l = 0
-
-        have, need = 0, len(tcount)
-        res, resl = [-1,-1], 10**6
-
-        for r in range(len(s)):
-            c = s[r]
-            window[c]=window.get(c,0) + 1
-            if c in t and window[c]==tcount[c]:
-                have += 1
-
-            while have == need:
-                if (r-l+1)<resl:
-                    res = [l,r]
-                    resl = r-l+1
-
-                window[s[l]]-=1
-                if s[l] in t and window[s[l]]<tcount[s[l]]:
-                    have-=1
-                l+=1
         
-        if resl==10**6:
+        tvals = {}
+        for i in t:
+            tvals[i] = 1 + tvals.get(i,0)
+        have, need = 0, len(tvals)
+        res, resl = [], 10**6
+        window = {}
+        l = 0
+        for i in range(len(s)):
+            # print(have, need, i)
+            window[s[i]]  = 1 + window.get(s[i], 0) 
+            if s[i] in t and window[s[i]]==tvals[s[i]]:
+                have += 1
+                
+            while have==need:
+                if resl>(i-l+1):
+                    res = [l,i]
+                    resl = i-l+1
+                window[s[l]] -= 1
+                if s[l] in t and window[s[l]]<tvals[s[l]]:
+                    have -= 1
+                l += 1
+        if resl == 10**6:
             return ""
         else:
             return s[res[0]:res[1]+1]
