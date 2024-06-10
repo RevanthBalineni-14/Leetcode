b5ac793393801d016e00490040a79d9d29e2a1f7
RevanthBalineni-14 <89599670+RevanthBalineni-14@users.noreply.github.com>
2024-06-10 16:53:26 +0530

    Time: 46 ms (15.94%), Space: 16.5 MB (77.64%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,10 +1,18 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        temp = heights
-        heights = sorted(heights)
-        count = 0
-        for ind,i in enumerate(temp):
-            if i!=heights[ind]:
+        freqheight = [0]*101
+        for i in heights:
+            freqheight[i] += 1
+        
+        crt, count = 0, 0
+        for i in heights:
+            
+            while freqheight[crt] == 0:
+                crt += 1
+            
+            freqheight[crt] -= 1
+            
+            if crt!=i:
                 count += 1
         
         return count
