af6ce15bba19f25942e52e5993a0225984a65a0b
Revanth Siva Sai Ram Balineni <revanthbalineni14@gmail.com>
2024-09-20 00:27:58 -0400

    Time: 47 ms (94.70%), Space: 17 MB (64.78%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -2,18 +2,10 @@ class Solution:
     def shortestPalindrome(self, s: str) -> str:
         if s=="":
             return s
-        def pallindromechecker(s):
-            if s==s[::-1]:
-                return True
-            else:
-                return False
-        
-        prefix = ""
         n = len(s)
-        for prefind in range(n-1, -1, -1):
-            if pallindromechecker(prefix+s):
-                return prefix+s
-            
-            prefix += s[prefind]
-            
+        revs = s[::-1]
+        for i in range(n):
+            if s.startswith(revs[i:]):
+                return revs[:i]+s
+
         return s+s
