1c52040a4e27ae19951b4fab484ce8dea4fede12
Revanth Siva Sai Ram Balineni <revanthbalineni14@gmail.com>
2024-10-20 02:16:21 -0400

    Time: 12 ms (100.00%), Space: 16.7 MB (80.84%) - LeetHub

---
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.py
@@ -0,0 +1,29 @@
+class Solution:
+    def parseBoolExpr(self, expression: str) -> bool:
+        st = deque()
+        for i in expression:
+            if i in [',', '(']:
+                continue
+            
+            if i in ['t', 'f', '&', '|', '!']:
+                st.append(i)
+            
+            if i == ')':
+                has_t = False
+                has_f = False
+                while st[-1] not in ['&', '|', '!']:
+                    temp = st.pop()
+                    if temp == 't':
+                        has_t = True
+                    elif temp =='f':
+                        has_f = True
+                op = st.pop()
+                if op == '&':
+                    st.append('f' if has_f else 't')
+                elif op == '|':
+                    st.append('t' if has_t else 'f')
+                else:
+                    st.append('t' if not has_t else 'f')
+        
+        return st[-1] == 't'
+                
