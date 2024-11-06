cafa92d0df6fcf69682854e480ef6d36edc56c39
Revanth Siva Sai Ram Balineni <revanthbalineni14@gmail.com>
2024-11-06 17:31:56 -0500

    Time: 17 ms (39.82%), Space: 16.8 MB (23.11%) - LeetHub

---
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        cmin = nums[0]
+        cmax = nums[0]
+        
+        tracker = []
+        
+        curr = str(bin(nums[0])).split('b')[1].count('1')
+        
+        for i in nums:
+            newcurr = str(bin(i)).split('b')[1].count('1')
+            
+            if newcurr == curr:
+                cmin = min(cmin, i)
+                cmax = max(cmax, i)
+                
+            else:
+                tracker.append([cmin, cmax])
+                curr = newcurr
+                cmin = i
+                cmax = i
+           
+        tracker.append([cmin, cmax])
+        print(tracker)
+        for i in range(1, len(tracker)):
+            if tracker[i-1][1] > tracker[i][0]:
+                return False
+            
+        return True
