8b4013723509c19b6ef94185fd112365444d385c
Revanth Siva Sai Ram Balineni <revanthbalineni14@gmail.com>
2024-11-30 19:54:05 -0500

    Time: 0 ms (100.00%), Space: 12.1 MB (5.40%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -1,3 +1,13 @@
-class Solution:
-    def minBitFlips(self, start: int, goal: int) -> int:
-        return bin(start ^ goal).count('1')
+class Solution(object):
+    def minBitFlips(self, start, goal):
+        """
+        :type start: int
+        :type goal: int
+        :rtype: int
+        """
+        res = 0
+        for i in range(32):
+            if (1<<i & start) != (1<<i & goal):
+                res += 1
+                
+        return res
