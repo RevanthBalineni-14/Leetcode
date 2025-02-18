7cb66ae504d4c194b79d5d653582faf4315b8193
Revanth Siva Sai Ram Balineni <revanthbalineni14@gmail.com>
2025-02-18 02:12:53 -0500

    Time: 0 ms (100%), Space: 17.7 MB (54.04%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.py
@@ -1,29 +1,12 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        carr = []
-        for i in range(1, len(pattern)+2):
-            carr.append(i)
+        res, temp= ["1"], []
+        for i in range(len(pattern)):
+            if pattern[i] == "I":
+                res += temp[::-1] + [str(i+2)]
+                temp = []
+            else:
+                temp += res.pop()
+                res += [str(i+2)]
 
-        res = float('inf')
-        def traverse(curr, carr):
-            nonlocal res
-
-            if len(curr)==(len(pattern)+1):
-                cpattern = ""
-                for index in range(1, len(curr)):
-                    if int(curr[index-1])<int(curr[index]):
-                        cpattern += "I"
-                    else:
-                        cpattern += "D"
-                if cpattern == pattern:
-                    res = min(res, int(curr))
-                    return True
-                else:
-                    return False
-            
-            for index, i in enumerate(carr):
-                if traverse(curr + str(i), carr[:index]+carr[index+1:]):
-                    return True
-
-        traverse("", carr)
-        return str(res)        
+        return "".join(res)+"".join(temp[::-1])
