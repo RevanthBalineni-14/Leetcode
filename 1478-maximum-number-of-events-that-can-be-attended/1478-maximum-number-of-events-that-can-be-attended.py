b27e4e1bc999dbe7d8d28d7d72e4eb2d36d89b61
Revanth Siva Sai Ram Balineni <revanthbalineni14@gmail.com>
2025-07-06 22:41:58 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.py
@@ -0,0 +1,17 @@
+import heapq
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        events = sorted(events, key = lambda x: x[0])
+        h = []
+        curr = 0
+        res = 0
+        for i in range(1, 10001):
+            while h and h[0]<i:
+                heapq.heappop(h)
+            while curr<len(events) and events[curr][0]==i:
+                heapq.heappush(h, events[curr][1])
+                curr+=1
+            if h:
+                heapq.heappop(h)
+                res+=1
+        return res
